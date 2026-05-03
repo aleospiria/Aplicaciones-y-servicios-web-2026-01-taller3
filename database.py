3f814b47d4b545ecdedacca4660c40db5f04a8dc
Aleospiria <alejoospi779@gmail.com>
2026-05-02 22:04:37 -0500

    Nueva dependencia para validacion de Emails

--- database.py
+++ database.py
@@ -3,8 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+# Cargar .env desde la raíz del proyecto
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
